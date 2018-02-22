4e6e221e1298da61862e2894ff41f56609462b69
Mariana BORTNICHUK <mbortnic@e1r8p18.unit.ua>
2018-02-22 11:29:52 +0200

    told ya, rewriting everything

--- ft_filler.h
+++ ft_filler.h
@@ -5,39 +5,57 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: mbortnic <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2018/02/20 10:57:55 by mbortnic          #+#    #+#             */
-/*   Updated: 2018/02/20 10:57:57 by mbortnic         ###   ########.fr       */
+/*   Created: 2018/02/21 13:09:57 by mbortnic          #+#    #+#             */
+/*   Updated: 2018/02/21 13:09:59 by mbortnic         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FT_FILLER_H
 # define FT_FILLER_H
 
-# include "libft/libft.h"
+# include "./libft/libft.h"
 
-typedef struct s_fillergame
+typedef struct s_map
 {
-	int		move_x;
-	int		move_y;
-	char	player;
-	char	enemy;
+	int		player;
+	char	*me;
+	int		x_me;
+	int		y_me;
+	char	**map;
 	int		x_cord;
 	int		y_cord;
-	char	**map;
-	int		x_figure;
-	int		y_figure;
-	char	**figure;
-	int		x_player;
-	int		y_player;
+	char	*enemy;
 	int		x_enemy;
 	int		y_enemy;
-	int		invalid;
-	int		ko;
-	int		alg;
-}				t_fillergame;
+	void	*victory;
+	int		x_first_pos;
+	int		y_first_pos;
+	char	*res;
+}				t_map;
 
-char	**ft_strnew_upd1(int x, int y);
-int		ft_take_symb(t_fillergame *sample, char *str);
+typedef struct s_figure
+{
+	char	**figure;
+	int		contact;
+	int		contact_nb;
+	int		x_size;
+	int		y_size;
+	int		x_size_real;
+	int		y_size_real;
+	int		x_first;
+	int		x_last;
+	int		y_first;
+	int		y_last;
+	int		x_temp;
+	int		y_temp;
+	int		finalx;
+	int		finaly;
+}				t_figure;
 
+void	ft_player_config(t_map *map);
+void	ft_take_player(t_map *map);
+void	ft_strategic_pos(t_map *map, char *str, int i);
+void	ft_take_map(t_map *map);
+void	ft_mapsize(t_map *map, char *str);
 
 #endif
---
+++ ft_utils.c
@@ -0,0 +1,120 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_utils.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mbortnic <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/02/21 13:37:21 by mbortnic          #+#    #+#             */
+/*   Updated: 2018/02/21 13:37:22 by mbortnic         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_filler.h"
+
+
+void	ft_player_config(t_map *map)
+{
+	if (map->player == 1)
+	{
+		map->me = "Oo";
+		map->enemy = "Xx";
+	}
+	if (map->player == 2)
+	{
+		map->me = "Xx";
+		map->enemy = "Oo";
+	}
+}
+
+void	ft_take_player(t_map *map)
+{
+	char	*str;
+
+	get_next_line(0, &str);
+	if (ft_strncmp(str, "$$$", 2) == 0 && map->player == 0)
+	{
+		if (ft_strstr(str, "p2"))
+			map->player = 2;
+		if (ft_strstr(str, "p1"))
+			map->player = 1;
+		ft_player_config(map);
+	}
+	free(&str);
+}
+
+void	ft_strategic_pos(t_map *map, char *str, int i)
+{
+	int			i;
+	static int	j;
+
+	if (j == map->y_cord)
+		return ;
+	j = 0;
+	while (++j < (map->y_cord))
+	{
+		i = 0;
+		while (++i < (map->x_cord))
+		{
+			if ((map->map[j][i] == map->enemy[0] || \
+				map->map[j][i] == map->enemy[1]))
+			{
+				map->x_enemy = i;
+				map->y_enemy = j;
+			}
+			if ((map->[j][i] == map->me[0] || \
+				map->map[j][i] == map->me[1]))
+			{
+				map->x_me = i;
+				map->y->me = j;
+			}
+		}
+	}
+}
+
+void	ft_take_map(t_map *map)
+{
+	int		i;
+	char	*str;
+
+	i = 0;
+	get_next_line(0, &str);
+	free(&str);
+	map->map = (char**)malloc(sizeof(char*) * (map->y_cord + 1));
+	// CHECK(map->map);
+	while (i <= (map->y_cord - 1))
+	{
+		get_next_line(1, &str);
+		map->map[i] = ft_strdup((const char*)(&str[4]));
+		free(&str);
+		i++;
+	}
+	ft_strategic_pos(map, str, i);
+}
+
+void	ft_mapsize(t_map *map, char *str)
+{
+	int		i;
+	int		temp;
+
+	i = 0;
+	map->x_cord = 0;
+	map->y_cord = 0;
+	while (str[i])
+	{
+		temp = 0;
+		while (ft_isdigit(str[i]) == 1 && str[i])
+		{
+			temp = temp + (str[i] - '0');
+			if (ft_isdigit(str[i + 1]) == 1)
+				temp = temp * 10;
+			i++;
+		}
+		if (map->y_cord == 0)
+			map->y_cord = temp;
+		else if (map->x_cord == 0)
+			map->x_cord = temp;
+		i++;
+	}
+	ft_take_map(map);
+}
